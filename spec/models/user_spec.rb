0291205bf06940ecc176c213636411f33a065073
Kon Ham <konham@MacBook-Pro.lan>
2022-01-11 20:36:12 -0500

    Create model validations for validation of presence of User columns; tests failing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:farm_name) }
+  end
+
   describe 'table relationship' do
     it { should have_many(:sub_locations) }
   end
