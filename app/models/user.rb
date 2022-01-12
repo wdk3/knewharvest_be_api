38d1fc126c30202e78a47b82c1da71b042c147ca
K. Ham <info.konham@gmail.com>
2022-01-11 20:39:42 -0500

    Include passing test assertions and model validations for User; spec requests failing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  # data validations
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :farm_name
+
+  # table relationship
   has_many :sub_locations
 end
